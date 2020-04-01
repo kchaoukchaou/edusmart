a55074ce7b4601ae51e8f780e4160d500fde8980
kchaou <kchaouyassine93@gmail.com>
2020-04-01 02:20:47 +0200

    first commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('mounir@gmail.com');
+        $password =password_hash('mounir1234', PASSWORD_ARGON2I);
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_SUPER_ADMIN']);
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
